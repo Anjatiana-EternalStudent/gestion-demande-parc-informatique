f906e47b9c786546728ed3b45805a80d3613cbd0
Anjatiana-EternalStudent <andriamanivoanja@gmail.com>
2026-02-19 16:37:29 +0300

    Comportement et permission de l'API tickets

--- tickets/views.py
+++ tickets/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render
 from .serializers import TicketSerializer
 from .models import Tickets
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission, SAFE_METHODS
 # Create your views here.
+class TicketPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return(
+                request.user.is_staff or
+                obj.demandeur == request.user or
+                obj.technicien == request.user
+            )
+        if request.user.is_staff:
+            return True
+        if obj.demandeur == request.user:
+            return True
+        if obj.technicien == request.user:
+            return True
+        if obj.demandeur == request.user and obj.technicien is None:
+            return True
+        return False
 
 class TicketViewSet(viewsets.ModelViewSet):
     serializer_class = TicketSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, TicketPermission]
 
     def get_queryset(self):
         user = self.request.user
@@ -19,3 +36,15 @@ class TicketViewSet(viewsets.ModelViewSet):
             return Tickets.objects.all()
     def perform_create(self, serializer):
         serializer.save(demandeur = self.request.user)
+
+    def perform_update(self, serializer):
+        user = self.request.user
+        ticket = self.get_object()
+
+        if ticket.technicien == user and not user.is_staff:
+            serializer.save(
+                demandeur=ticket.demandeur,
+                technicien=ticket.technicien
+            )
+        else:
+            serializer.save()
