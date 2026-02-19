d8beb728e39c22c44b951e6ec8addf93c8fc5d2d
Anjatiana-EternalStudent <andriamanivoanja@gmail.com>
2026-02-19 15:27:30 +0300

    Ajout du model User au panneau administateur django

--- utilisateurs/admin.py
+++ utilisateurs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Role', {'fields':('role',)}),
+    )
+    list_display = ('username', 'email','role', 'is_staff', 'is_superuser', 'date_creation')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    list_editable = ('role',)
+
+    search_fields = ('username', 'email')
+admin.site.register(User, UserAdmin)
